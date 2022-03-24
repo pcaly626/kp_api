439a864003486b8db6e48d7a98bbca09399c2d4c
paul <paul.clay@fleshlight.com>
2022-03-24 09:28:25 -0500

    Add Readme for Rendering Data to the front end

--- static/helloworld/js/population.js
+++ static/helloworld/js/population.js
@@ -1,49 +1,13 @@
 
- async function get_singapore_data(){
+ const  get_singapore_one_data = async () =>{
 
-    let response = await fetch('http://localhost:8000/pop/',{method:"GET",headers:{'Content-Type':'application/json'}})
+    let response = await fetch('http://localhost:8000/pop/1',{method:"GET",headers:{'Content-Type':'application/json'}})
     const data = response.json()
     return data;
 }
 
-get_singapore_data().then( data => {
-    const names = Object.keys(data.dataset_one).map( year => { return data.dataset_one[year].map( d => d.name)})
-    const populations = Object.keys(data.dataset_one).map( year => { return data.dataset_one[year].map( d => d.value)})
-    const ctx = document.getElementById('myChart').getContext('2d');
-const myChart = new Chart(ctx, {
-    type: 'bar',
-    data: {
-        labels: [...names[0]],
-        datasets: [{
-            label: '# of Votes',
-            data: [...populations[0]],
-            backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)',
-                'rgba(75, 192, 192, 0.2)',
-                'rgba(153, 102, 255, 0.2)',
-                'rgba(255, 159, 64, 0.2)'
-            ],
-            borderColor: [
-                'rgba(255, 99, 132, 1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)'
-            ],
-            borderWidth: 1
-        }]
-    },
-    options: {
-        scales: {
-            y: {
-                beginAtZero: true
-            }
-        }
-    }
-});
+
+get_singapore_one_data().then( data => {
+    document.querySelector('#district').append(data.name)
+    document.querySelector('#population').append(data.population)
 })
-
-
